c19eeb7b7ce8e0b83afb205f535daa812e8b0d3b
fore447 <mmori.0426@gmail.com>
2023-11-03 13:54:43 +0900

    👍[TOP]セルのレイアウトを作成した
    【対応内容】
    ・セルのレイアウトを作成した
    
    【影響範囲】
    ・TOP
    
    【動作確認】
    ・セルがのレイアウトが崩れない

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            Image("GitHubMark")
+                .resizable()
+                .frame(width: 44.0, height: 44.0)
+            VStack(alignment: .leading) {
+                Text("Owner Name")
+                    .font(.caption)
+                Text("Good evening, world!")
+                    .font(.body)
+                    .fontWeight(.semibold)
+            }
         }
-        .padding()
     }
 }
 
